4463f1cb7622a4d856aa7edf33c858eb02d2d7d6
EdsonJunior <edsonjos61@gmail.com>
2024-09-22 10:40:00 -0300

    Creating unit tests for the button and form

--- src/components/login/__tests__/loginForm.spec.tsx
+++ src/components/login/__tests__/loginForm.spec.tsx
@@ -1,24 +1,85 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { LoginForm } from '../loginForm';
+
+jest.mock('next-auth/react', () => ({
+  signIn: jest.fn(),
+}));
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('Render Login Form component', () => {
   it('should be rendered as the word email', () => {
     render(<LoginForm />);
-    screen.getByLabelText('Email');
+    const wordEmail = screen.getByLabelText('Email');
+
+    expect(wordEmail).toBeInTheDocument();
   });
 
   it('should be rendered a placeholder email', () => {
     render(<LoginForm />);
-    screen.getByPlaceholderText('Email');
+    const wordEmailPlaceholder = screen.getByPlaceholderText('Email');
+
+    expect(wordEmailPlaceholder).toBeInTheDocument();
   });
 
   it('should be rendered as the word password', () => {
     render(<LoginForm />);
-    screen.getByLabelText('Password');
+    const wordPassword = screen.getByLabelText('Password');
+
+    expect(wordPassword).toBeInTheDocument();
   });
 
   it('should be rendered a placeholder password', () => {
     render(<LoginForm />);
-    screen.getByPlaceholderText('Password');
+    const wordPasswordPlaceholder = screen.getByPlaceholderText('Password');
+
+    expect(wordPasswordPlaceholder).toBeInTheDocument();
+  });
+
+  it('should be rendered a button', () => {
+    render(<LoginForm />);
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it('should disable submit button when form is invalid', async () => {
+    render(<LoginForm />);
+
+    // Verifica se os inputs e o botão estão no documento
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+
+    // Preenche o e-mail e password com um formato inválido
+    fireEvent.change(emailInput, { target: { value: 'invalidemail' } });
+    fireEvent.change(passwordInput, { target: { value: 'P@ssw0rd' } });
+
+    // Aguarda a validação do formulário.
+    // Verifica se o botão está desabilitado pois, os dados de email e senha estão inválidos
+    await waitFor(() => {
+      expect(submitButton).toBeDisabled();
+    });
+  });
+
+  it('should enable the submit button when the form is valid', async () => {
+    render(<LoginForm />);
+
+    // Verifica se os inputs e o botão estão no documento
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+
+    // Preenche o e-mail e password com um formato válido
+    fireEvent.change(emailInput, { target: { value: 'user@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'P@ssw0rd1' } });
+
+    // Aguarda a validação do formulário.
+    // Verifica se o botão está habilitado pois, os dados de email e senha estão válidos
+    await waitFor(() => {
+      expect(submitButton).not.toBeDisabled();
+    });
   });
 });
